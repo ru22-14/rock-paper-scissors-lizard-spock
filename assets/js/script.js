f3a71d85b2987bb16d1fad1aaf1f24069de87e9a
rohina usman <rohina.usman@gmail.com>
2023-02-07 10:09:37 +0000

    add Eventlistener and main function of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-console.log(connected);
+/** declare possible choices and const from DOM */
+
+const pScore = document.getElementById("p-score");
+const cScore = document.getElementById("c-score");
+const buttons =document.getElementsByClassName("choice");
+const choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+
+/** event listener */
+
+for (let button of buttons) {
+    button.addEventListener("click", function(){
+        let playerChoice = this.getAttribute("data-choice");
+
+        
+        playGame(playerChoice);
+
+    });
+
+}
+
+/** Game working function */
+
+function playGame(playerChoice) {
+    let computerChoice = choices[Math.floor(Math.random()*5)];
+
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+    gameScores(result);
+
+}
