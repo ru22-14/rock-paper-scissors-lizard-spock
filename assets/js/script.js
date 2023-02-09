6095174492fc775fa56b19d1a10e9dea288f98db
rohina usman <rohina.usman@gmail.com>
2023-02-09 20:44:55 +0000

    computer choice function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@
 
 let playerScore = 0;
 let computerScore = 0;
-const playerScore_span  = document.getElementById("player-score");
-const computerScore_span  = document.getElementById("computer-score");
+const playerScore_span = document.getElementById("player-score");
+const computerScore_span = document.getElementById("computer-score");
 const result_h3 = document.getElementById("result");
 const rock_button = document.getElementById("btn-1");
 const paper_button = document.getElementById("btn-2");
@@ -18,32 +18,43 @@ const spock = "spock";
 const choice = document.getElementById("choice");
 
 /**  EventListener function for all buttons */
-function main (){
-   
-    rock_button.addEventListener('click', function() {
-     // game(rock_button);
-     game(rock_button);
+function main() {
+
+    rock_button.addEventListener('click', function () {
+        // game(rock_button);
+        game(rock_button);
+
+
+    });
+
+    paper_button.addEventListener('click', function () {
+        game(paper_button);
+
+    });
+
+    scissors_button.addEventListener('click', function () {
+        game(scissors_button);
+    });
+
+    lizard_button.addEventListener('click', function () {
+        game(lizard_button);
+    });
+
+    spock_button.addEventListener('click', function () {
+        game(spock_button);
+    });
+
+}
+
+main();
+
+/**function to get the computer choice */
+
+function getComputerChoice() {
+    const choices = ['rock','paper','scissors','lizard','spock'];
+    const randomNumber = Math.floor(Math.random() *5);
+    return choices[randomNumber];
     
-     
-    });
- 
-    paper_button.addEventListener('click', function() {
-     game(paper_button);
-     
-    });
- 
-    scissors_button.addEventListener('click', function() {
-     game(scissors_button);
-    });
- 
-    lizard_button.addEventListener('click', function() {
-     game(lizard_button);
-    });
- 
-    spock_button.addEventListener('click', function() {
-     game(spock_button);
-    });
- 
- }
- 
- main();
+    
+}
+console.log(getComputerChoice());
