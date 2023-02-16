c1a84b65eb95f06456b5e93a41c0341a50735331
rohina usman <rohina.usman@gmail.com>
2023-02-16 12:52:18 +0000

    comments added and if else statement for tie is added

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,208 +10,256 @@ const paper_button = document.getElementById("btn-2");
 const scissors_button = document.getElementById("btn-3");
 const lizard_button = document.getElementById("btn-4");
 const spock_button = document.getElementById("btn-5");
-const rock = "rock";
-const paper = "paper";
-const scissors = "scissors";
-const lizard = "lizard";
-const spock = "spock";
 
 
 
 
-/**function to get the computer choice */
+
+/* The purpose of this function is to brings the computer's choice randomly 
+ * throughout the game is played */
 
 function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+    const choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
     const randomNumber = Math.floor(Math.random() * 5);
     return choices[randomNumber];
 
-
 }
-console.log(getComputerChoice());
 
-/** win function will display the game winner */
+/** win function will calculate the scores and will display the game winner */
 
- function win() {
-     playerScore++;
-     playerScore_span.innerHTML = playerScore;
-     computerScore_span.innerHTML = computerScore;
-     if (playerScore === 5) {
-       result_h3.innerHTML = (`You Win`);
+function win(playerstr, computerstr) {
+    playerScore++;
+    playerScore_span.innerHTML = playerScore;
+    computerScore_span.innerHTML = computerScore;
+    if (playerScore === 5) {
         playerScore = 0;
         computerScore = 0;
-     }
-}     
-  /** loose function will display who looses the game  */
-function loose() {
+        playerScore_span.innerText = playerScore;
+        computerScore_span.innerText = computerScore;
+        result_h3.innerHTML = (`You Win, Start Again`);
+    }
+}
+
+/** loose function will calculate who looses the game  */
+function loose(playerstr, computerstr) {
     computerScore++;
     playerScore_span.innerHTML = playerScore;
     computerScore_span.innerHTML = computerScore;
     if (computerScore === 5) {
-        result_h3.innerHTML = (`Computer Wins`);
         playerScore = 0;
         computerScore = 0;
+        playerScore_span.innerText = playerScore;
+        computerScore_span.innerText = computerScore;
+        result_h3.innerHTML = (`Computer Wins, Try Again`);
     }
 
+
 }
-
-
-
-/** main game function */
+/*This is the main function of the Game. Everytime the player clicks on the button
+the data is taken and compared with the computer's choice and will get the 
+scores through win or loose function and will display a text accordingly. */
 
 function game(playerChoice) {
-    
 
-    const computerChoice = getComputerChoice();
+    let computerChoice = getComputerChoice();
+    let str1 = playerChoice.innerText;
+    let str2 = computerChoice.innerText;
 
-    if (playerChoice === computerChoice) {
-        console.log("it's a Tie");
+    if (playerChoice === rock_button && computerChoice === 'Rock') {
+        result_h3.innerHTML = (`${playerChoice.innerText} is equals to ${computerChoice} its a Tie.`);
+
+
+    } else if (playerChoice === paper_button && computerChoice === 'Paper') {
+        result_h3.innerHTML = (`${playerChoice.innerText} is equals to ${computerChoice} its a Tie.`);
+
+    } else if (playerChoice === scissors_button && computerChoice === 'Scissors') {
+        result_h3.innerHTML = (`${playerChoice.innerText} is equals to ${computerChoice} its a Tie.`);
+
     } else if (playerChoice === rock_button) {
-        if (computerChoice === paper) {
-            console.log("you loose");
-            loose();
-        }else {
-            win();
-        }
-    } else if (playerChoice ===  rock_button) {
-        if (computerChoice === scissors) {
-            win();
-            
-            console.log("you win");
-           
-        } else{
-            loose();
-        }
-    } else if (playerChoice === rock_button) {
-        if (computerChoice === lizard) {
-            win();
-            console.log("you win");
-            
+        if (computerChoice === 'Paper') {
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
         } else {
-            loose();
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
         }
     } else if (playerChoice === rock_button) {
-        if (computerChoice === spock) {
-            loose();
-            
+        if (computerChoice === 'Scissors') {
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
+
+
+
         } else {
-            win();
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
+        }
+    } else if (playerChoice === rock_button) {
+        if (computerChoice === 'Lizard') {
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
+
+
+        } else {
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
+        }
+    } else if (playerChoice === rock_button) {
+        if (computerChoice === ' Spock') {
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
+
+        } else {
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
         }
     } else if (playerChoice === paper_button) {
-        if (computerChoice === rock) {
-            win();
-            
+        if (computerChoice === 'Rock') {
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
+
         } else {
-            loose();
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
         }
     } else if (playerChoice === paper_button) {
-        if (computerChoice === scissors) {
-            loose();
-           
+        if (computerChoice === 'Scissors') {
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
+
         } else {
-            win();
+            win(str1, str2);
         }
     } else if (playerChoice === paper_button) {
-        if (computerChoice === lizard) {
-            loose();
-            
+        if (computerChoice === 'Lizard') {
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
+
         } else {
-            win();
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
         }
     } else if (playerChoice === paper_button) {
-        if (computerChoice === spock) {
-            win();
-            
+        if (computerChoice === 'Spock') {
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
+
         }
     } else if (playerChoice === scissors_button) {
-        if (computerChoice === rock) {
-            loose();
-            
+        if (computerChoice === 'Rock') {
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
+
         } else {
-            win();
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
         }
     } else if (playerChoice === scissors_button) {
-        if (computerChoice === paper) {
-            win();
-            
+        if (computerChoice === 'Paper') {
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
+
         } else {
-            loose();
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
         }
     } else if (playerChoice === scissors_button) {
-        if (computerChoice === lizard) {
-            win();
-            
+        if (computerChoice === 'Lizard') {
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
+
         } else {
-            loose();
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
         }
     } else if (playerChoice === scissors_button) {
-        if (computerChoice === spock) {
-            loose();
-            
+        if (computerChoice === 'Spock') {
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
+
         } else {
-            win();
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
         }
     } else if (playerChoice === lizard_button) {
-        if (computerChoice === rock) {
-            loose();
-            
+        if (computerChoice === 'Rock') {
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
+
         } else {
-            win();
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
         }
     } else if (playerChoice === lizard_button) {
-        if (computerChoice === paper) {
-            win();
-        
+        if (computerChoice === 'Paper') {
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
+
         } else {
-            loose();
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
         }
     } else if (playerChoice === lizard_button) {
-        if (computerChoice === scissors) {
-            loose();
-            
+        if (computerChoice === 'Scissors') {
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
+
         } else {
-            win();
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
         }
     } else if (playerChoice === lizard_button) {
-        if (computerChoice === spock) {
-            win();
-           
+        if (computerChoice === 'Spock') {
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
+
         } else {
-            loose();
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
         }
     } else if (playerChoice === spock_button) {
-        if (computerChoice === rock) {
-            win();
-           
+        if (computerChoice === 'Rock') {
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
+
         } else {
-            loose();
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
         }
     } else if (playerChoice === spock_button) {
-        if (computerChoice === paper) {
-            loose();
-            
+        if (computerChoice === 'Paper') {
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
+
         } else {
-            win();
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
         }
     } else if (playerChoice === spock_button) {
-        if (computerChoice === scissors) {
-            win();
-           
+        if (computerChoice === 'Scissors') {
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
+
         } else {
-            loose();
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
         }
     } else if (playerChoice === spock_button) {
-        if (computerChoice === lizard) {
-            loose();
-            
+        if (computerChoice === 'Lizard') {
+            result_h3.innerHTML = (`${computerChoice} beats ${playerChoice.innerText} `);
+            loose(str1, str2);
+
         } else {
-            win();
+            result_h3.innerHTML = (`${playerChoice.innerText} beats ${computerChoice} `);
+            win(str1, str2);
         }
     }
 
 }
 
-/**  EventListener function for all buttons */
+/**  EventListener function for all buttons to get the information which button is being clicked
+     and then work accordingly*/
+
 function main() {
 
     rock_button.addEventListener('click', function () {
@@ -237,7 +285,6 @@ function main() {
     spock_button.addEventListener('click', function () {
         game(spock_button);
     });
-
 }
 
 main();
